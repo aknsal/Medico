859079d8cdda3230e544c89668ab4464bc1bdce5
Mudit018 <agarwalmudit018@gmail.com>
2021-10-28 01:46:05 +0530

    Added Home Cards

---
+++ src/components/cards/Card1.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import classes from './Card1.module.css'
+
+const Card1 = () => {
+    return (
+        <div className={classes.main}>
+            <div className={classes.text}>
+                <h1>Consult Top Doctors Online For</h1>
+                <h1>Any Health Concern</h1>
+            </div>
+            <div className={classes.container}>
+                <li className={classes.item}>
+                    <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                    <p>Baby Health</p>
+                </li>
+                <li className={classes.item}>
+                    <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                    <p>Stomach</p>
+                </li>
+                <li className={classes.item}>
+                    <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                    <p>Psychiatry</p>
+                </li>
+                <li className={classes.item}>
+                    <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                    <p>Urology</p>
+                </li>
+                <li className={classes.item}>
+                    <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                    <p>Dermatology</p>
+                </li>
+                <li className={classes.item}>
+                    <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                    <p>Infectious Diseases</p>
+                </li>
+            </div>
+        </div>
+    )
+}
+
+export default Card1
