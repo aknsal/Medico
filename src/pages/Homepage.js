323f002d94e8ed7af9bb54b469f8776a6b8d424f
Akshaj Kansal <akshajkansal@yahoo.com>
2021-10-28 22:38:40 +0530

    Merge Homepage Components

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,18 @@
 import React from "react";
+import FAQ from "../components/faq/FAQ";
+import TopSection from "../components/topSection/TopSection";
+import Card1 from "../components/cards/Card1";
+import Card2 from "../components/cards/Card2";
+import Card3 from "../components/cards/Card3";
 
 export default function Homepage() {
-  return <div></div>;
+  return (
+    <div>
+      <TopSection />
+      <Card1 />
+      <Card2 />
+      <Card3 />
+      <FAQ />
+    </div>
+  );
 }
