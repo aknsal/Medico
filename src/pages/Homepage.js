799eb752e98347255bba15f60169b48125da44c8
Priyal Saxena <priyalsaxena66@gmail.com>
2021-10-29 22:15:16 +0530

    added reviews, and pg to search drs

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,15 +4,17 @@ import TopSection from "../components/topSection/TopSection";
 import Card1 from "../components/cards/Card1";
 import Card2 from "../components/cards/Card2";
 import Card3 from "../components/cards/Card3";
+import Reviews from "../components/reviews/reviews";
 
 export default function Homepage() {
-  return (
-    <div>
-      <TopSection />
-      <Card1 />
-      <Card2 />
-      <Card3 />
-      <FAQ />
-    </div>
-  );
+	return (
+		<div>
+			<TopSection />
+			<Card1 />
+			<Card2 />
+			<Card3 />
+			<FAQ />
+			<Reviews />
+		</div>
+	);
 }
