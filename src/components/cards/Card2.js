5b116606dfba9037be752bbc5549d6fb70fcc909
Mudit018 <agarwalmudit018@gmail.com>
2021-10-31 04:24:33 +0530

    replaced text

--- src/components/cards/Card2.js
+++ src/components/cards/Card2.js
@@ -1,42 +1,44 @@
 import React from 'react'
 import classes from './Card2.module.css'
+import image1 from "../../assets/images/doctor.jpg";
+import image2 from "../../assets/images/doctorVideo.jpg"
 
 const Card2 = () => {
     return (
         <div className={classes.container}>
             <div className={classes.first}>
-                <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                <img className={classes.img} src={image1} alt="" />
                 <div className={classes.text}>
                     <h1>
                         Your Nearest Doctor
                     </h1>
                     <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, itaque aliquam natus possimus consequatur minima nihil incidunt est. Sapiente, nesciunt?
+                        Select preffered doctor and time slot to book an appointment or video consultation
                     </p>
                 </div>
             </div>
             <div className={classes.second}>
-                <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                <img className={classes.img} src={image2} alt="" />
                 <div className={classes.text}>
                     <h1>
                         Urgent Online Care
                     </h1>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quia iusto eius magni tenetur? Est perspiciatis odio molestiae adipisci laboriosam.
+                        Tell us your health and we will assign a top doctor to you.
                     </p>
                 </div>
             </div>
-            <div className={classes.first}>
+            {/* <div className={classes.first}>
                 <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
                 <div className={classes.text}>
                     <h1>
-                        Your Nearest Doctor
+                        What our patients say
                     </h1>
                     <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, itaque aliquam natus possimus consequatur minima nihil incidunt est. Sapiente, nesciunt?
+                        
                     </p>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/components/cards/Card3.js
+++ src/components/cards/Card3.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import classes from './Card3.module.css'
+import image1 from '../../assets/images/search.png'
+import image2 from '../../assets/images/docprofile.png'
+import image3 from '../../assets/images/calender.png'
+import image4 from '../../assets/images/solution.png'
 
 const Card3 = () => {
     return (
@@ -9,22 +13,22 @@ const Card3 = () => {
             </div>
             <div className={classes.container}>
                 <li className={classes.item}>
-                    <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                    <img className={classes.img} src={image1} alt="doctor" />
                     <p className={classes.heading}>Search Doctor</p>
                     <p className={classes.text1}>We're here to help whenever you feel ill, but keeping you healthy is our better priority</p>
                 </li>
                 <li className={classes.item}>
-                    <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                    <img className={classes.img} src={image2} alt="profile" />
                     <p className={classes.heading}>Check Doctor Profile</p>
                     <p className={classes.text1}>We can help you find available vaccine appointments near you or notify you when availability</p>
                 </li>
                 <li className={classes.item}>
-                    <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                    <img className={classes.img} src={image3} alt="calender" />
                     <p className={classes.heading}>Schedule Appointment</p>
                     <p className={classes.text1}>From seasonal allergies to burn identification and treatments, we have the resources.</p>
                 </li>
                 <li className={classes.item}>
-                    <img className={classes.img} src="https://imgr.search.brave.com/mXi8pDr4RjpIgW_Gnyb487gHHLQcuugz6TQiPCIZT9A/fit/711/225/ce/1/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/dlNXdFJkX29QUlR3/RG9UQ21rUDVnSGFF/OCZwaWQ9QXBp" alt="" />
+                    <img className={classes.img} src={image4} alt="solution" />
                     <p className={classes.heading}>Get Your Solution</p>
                     <p className={classes.text1}>We can help you find available vaccine appointments near you or notify you when availability.</p>
                 </li>
